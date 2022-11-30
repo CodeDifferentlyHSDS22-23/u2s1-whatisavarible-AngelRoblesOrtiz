c1e5c6ba90088077215e354f62ce164f1799d87b
Roger Cronin <roger.m.cronin@gmail.com>
2022-11-29 19:53:17 -0500

    Completed assignment

---
+++ src/main/java/com/codedifferently/lab/partA/Variables01.java
@@ -0,0 +1,15 @@
+package com.codedifferently.lab.partA; //assuming this contains everything
+// empty line of code
+    public class Variables01 { //not exactly sure what this does yet, but I know it's necessary for the code to work
+        public static void main(String[] args) { //same thing here, not sure yet of it, but I know its important
+            String x = "Sam"; //the variable placeholder
+            System.out.println("My name is " +x); //first printed statement here, words the +x make the variable be plugged in
+            System.out.println(x +" is so cool!"); //second printed statement but this one starts with the variable instead of ending with it
+            System.out.println("Hooray " +x +"!"); // third printed statement and this one has the variable in the middle of it
+            System.out.println("Please enjoy " +x +" Appreciation " //the start of a long chain of a single printed statement
+                    + "Day! My name is " +x +". I know how excited " // these don't use parenthesis, they just tack it all into one pair of parenthesis at the very end
+                    + "everyone is to start appreciating " +x +" on " +x // doesn't even have to end with quotation marks
+                    + " Appreciation Day! " +x +", " +x +", " +x +"! Yay " //this one uses the variable multiple times within it
+                    + x +"!!! That's me! " +x +" is the best date ever!"); //the final line of the print statement
+        } // contains everything since its other pair
+    } // contains everything since its other pair
--- src/main/java/com/codedifferently/lab/partA/Variables02.java
+++ src/main/java/com/codedifferently/lab/partA/Variables02.java
@@ -1,16 +1,16 @@
-package com.codedifferently.lab.partA;
-
-public class Variables02 {
-    public static void main(String[] args) {
-        //RUN THE PROGRAM BETWEEN EACH STEP AND OBSERVE THE OUTPUT
-        //Step 1) Change the print statements in lines 9 and 13 so that they print the value of x.
-        //Step 2) Change the value of x to "kitty".
-        String x = "puppy";
-        System.out.println("puppy");
-
-        //Step 3) Add a line of code that changes the value of x to "bunny".
-
-        System.out.println("puppy");
-
-    }
-}
+package com.codedifferently.lab.partA; //assuming this is what contains everything
+// empty line of code
+public class Variables02 { //again, not sure what this does but, I know its important
+    public static void main(String[] args) { //^^
+        //RUN THE PROGRAM BETWEEN EACH STEP AND OBSERVE THE OUTPUT //comment
+        //Step 1) Change the print statements in lines 12 and 16 so that they print the value of x. //comment
+        //Step 2) Change the value of x to "kitty". //comment
+        String x = "kitty"; //the variable placeholder
+        System.out.println("" +x); // the first printed statement
+//empty line of code
+        //Step 3) Add a line of code that changes the value of x to "bunny". //don't think we do this one
+//empty line of code
+        System.out.println("puppy"); // second printed statement
+//empty line of code
+    } //contains everything since its last pair
+} //contains everything since its last pair
