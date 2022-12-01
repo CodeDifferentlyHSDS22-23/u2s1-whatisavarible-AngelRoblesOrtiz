b4950f844224a1581bbe3be5e267e235dea992ab
Roger Cronin <roger.m.cronin@gmail.com>
2022-12-01 17:20:39 -0500

    Completed lab

--- src/main/java/com/codedifferently/lab/partA/Variables01.java
+++ src/main/java/com/codedifferently/lab/partA/Variables01.java
@@ -10,6 +10,6 @@ package com.codedifferently.lab.partA; //assuming this contains everything
                     + "Day! My name is " +x +". I know how excited " // these don't use parenthesis, they just tack it all into one pair of parenthesis at the very end
                     + "everyone is to start appreciating " +x +" on " +x // doesn't even have to end with quotation marks
                     + " Appreciation Day! " +x +", " +x +", " +x +"! Yay " //this one uses the variable multiple times within it
-                    + x +"!!! That's me! " +x +" is the best date ever!"); //the final line of the print statement
+                    + x +"!!! That's me! " +x +" is the best date ever!!"); //the final line of the print statement
         } // contains everything since its other pair
     } // contains everything since its other pair
